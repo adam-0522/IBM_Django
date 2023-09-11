ce359c21aa1375b757e6da74765144df815116c5
“AdamC” <blouderock@gmail.com>
2023-09-10 20:53:11 -0700

    new user registration functionality added

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),
     # Authentication related urls
     path('logout/', views.logout_request, name='logout'),
-	
+	path('login/', views.login_request, name='login'),
+    path('registration/', views.registration_request, name='registration'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
