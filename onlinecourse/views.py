d792734a4db4960d9a79685c81d96c4f2eafd975
“AdamC” <blouderock@gmail.com>
2023-09-10 19:58:48 -0700

    updated class based views for enrollment and course list

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -7,9 +7,26 @@ from django.views import generic, View
 from django.http import Http404
 
 # Create your class based views here.
+# Note that we are subclassing CourseListView from base View class
+class CourseListView(View):
 
+    # Handles get request
+    def get(self, request):
+        context = {}
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html', context)
 
+class EnrollView(View):
 
+    # Handles post request
+    def post(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase total enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 # Function based views
 
 # Function-based course list view
